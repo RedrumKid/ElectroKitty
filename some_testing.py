63b3a53b0a96a583888f81abd27a806b844138b8
Ožbej Vodeb <ozbej.vodeb@gmail.com>
2026-04-21 15:17:37 +0200

    changelog added

--- some_testing.py
+++ some_testing.py
@@ -5,16 +5,16 @@ problem = ElectroKitty()
 
 a = [[np.float64(0.5), np.float64(0.5), np.float64(0.)]]
 
-problem.create_simulation(a, [293, 0, 0, np.float32(10**-4)], 2*[10**-9], [], [0.0001, 10, 10**-5, 0], [[], [10, 0]])
+problem.create_simulation(a, [293, 0, 0, np.float32(10**-4)], 2*[10**-9], [], [0.0001, 10, 10**-5, 0], [[], [np.float16(10), 0]])
 problem.V_potential(0.5, -0.5, 0.05, 0, 0, 1000)
 e, i, t = problem.simulate()
 problem.Plot_simulation()
 problem.set_data(e, i, t)
 
-# problem.fit_to_data(algorithm = "CMA-ES", tolx = 10**-2, tolf=10**-2)
-# problem.sample_parameter_distribution(n_samples = 60)
+problem.fit_to_data(algorithm = "CMA-ES", tolx = 10**-2, tolf=10**-2)
+problem.sample_parameter_distribution(n_samples = 60)
 
-# problem.FFT_analyze_sim(10, 2, [1, 1, 1, 1])
+problem.FFT_analyze_sim(10, 2, [1, 1, 1, 1])
 
 dict = problem.save_json("justatest")
 # print(dict.keys())
@@ -22,3 +22,4 @@ dict = problem.save_json("justatest")
 p1 = ElectroKitty()
 p1.load_from_json("justatest.json")
 p1.Plot_simulation()
+p1.save_json("test1")
